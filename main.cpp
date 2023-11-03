b868ab531b1797a29f00ce3b0fe41b9e6a1e9d3a
Paulo Bodaneze Reva <paulo_bodaneze@hotmail.com>
2023-11-03 19:08:53 -0300

    array initialization

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <time.h>
+#include <chrono>
+
+#define ARRAY_SIZE 1000
+#define MAX_NUMBER 5000
+
+void initArray(int * arr);
+
 int main(){
+    srand (time(NULL));
+
+    int randomNumberArray[ARRAY_SIZE];
+    initArray(randomNumberArray);
+
     return 0;
+}
+
+void initArray(int * arr){
+    auto start = std::chrono::steady_clock::now();
+
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        arr[i] = rand() % MAX_NUMBER + 1;
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::cout << "Time to generate array of size " << ARRAY_SIZE << ":" << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " nanoseconds.";
 }
